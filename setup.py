20789fd39f03107ff3870f61ff2fa02181689df5
Dmitry Maslennikov <mrdaimor@gmail.com>
2022-12-06 11:07:38 +0400

    version updated

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 from setuptools import find_namespace_packages, setup
+import os
 
 package_name = "dbt-iris"
 # make sure this always matches dbt/adapters/{adapter}/__version__.py
-package_version = "1.3.0b1"
+package_version = "1.3.0b3"
 description = """The InterSystems IRIS adapter plugin for dbt"""
+
+thelibFolder = os.path.dirname(os.path.realpath(__file__))
+requirementPath = thelibFolder + '/requirements.txt'
+
+requirements = []
+if os.path.isfile(requirementPath):
+    with open('./requirements.txt') as f:
+        for line in f.read().splitlines():
+            requirements.append(line)
 
 setup(
     name=package_name,
@@ -17,19 +27,17 @@ setup(
     url="https://github.com/caretdev/dbt-iris",
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
-    install_requires=[
-        "dbt-core~=1.3.0",
-    ],
+    install_requires=requirements,
     classifiers=[
         'Development Status :: 3 - Alpha',
 
-        'License :: OSI Approved :: MIT License'
+        'License :: OSI Approved :: MIT License',
         
-        'Operating System :: OS Independent'
+        'Operating System :: OS Independent',
 
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
     ],
-    python_requires=">3.7,<=3.10",
+    python_requires=">3.7,<3.11",
 )
