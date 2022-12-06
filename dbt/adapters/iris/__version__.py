c3c0977070ad77107221c636693a339d29c5a487
Dmitry Maslennikov <mrdaimor@gmail.com>
2022-12-06 10:15:15 +0400

    beta version

--- dbt/adapters/iris/__version__.py
+++ dbt/adapters/iris/__version__.py
@@ -1,1 +1,1 @@
-version = "1.3.0-b1"
+version = "1.3.0b1"
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_namespace_packages, setup
 
 package_name = "dbt-iris"
 # make sure this always matches dbt/adapters/{adapter}/__version__.py
-package_version = "1.3.0"
+package_version = "1.3.0b1"
 description = """The InterSystems IRIS adapter plugin for dbt"""
 
 setup(
@@ -11,6 +11,7 @@ setup(
     version=package_version,
     description=description,
     long_description=description,
+    license='MIT',
     author="CaretDev",
     author_email="info@caretdev.com",
     url="https://github.com/caretdev/dbt-iris",
@@ -19,4 +20,16 @@ setup(
     install_requires=[
         "dbt-core~=1.3.0",
     ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'License :: OSI Approved :: MIT License'
+        
+        'Operating System :: OS Independent'
+
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires=">3.7,<=3.10",
 )
